0620c634f2896d57e812bb6fa00856da5a32da2f
GPMunk <ik@munkgp.nl>
2023-12-28 16:26:12 +0100

    WIP Day8

---
+++ src/Days/Y2023/Day8/Solution.cs
@@ -0,0 +1,17 @@
+﻿using AdventOfCode.Infrastructure;
+
+namespace AdventOfCode.Y2023.Day8
+{
+    public class Solution : Solver
+    {
+        public override SolveResult SolvePartOne(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override SolveResult SolvePartTwo(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
